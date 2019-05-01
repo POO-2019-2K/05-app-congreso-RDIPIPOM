f7c6a74d6a030fd463ce59d9488c8b5c459dbcf6
RDIPIPOM <ezamora2@ucol.mx>
2019-05-01 13:03:32 -0500

    Tercera  Actualización

--- Ejercicio/Main.js
+++ Ejercicio/Main.js
@@ -1,12 +1,10 @@
 import TableMembers from './TableMembers.js';
 import TableCourses from './TableCourses.js';
 
-
-
 export default class Main {
     constructor() {
         let courses = new Array();
-        if(localStorage.getItem('courses') != null){
+        if (localStorage.getItem('courses') != null) {
             courses = JSON.parse(localStorage.getItem('courses'));
         }
 
@@ -14,17 +12,26 @@ export default class Main {
         let tableCourses = new TableCourses(document.querySelector('#tableCourses'));
 
         document.querySelector('#btnAdd').addEventListener('click', () => {
-            //Create object
-            let objCourse = this._createObjCourse();
-            //Add and save in LocalStorange
-            courses.push(objCourse);
-            localStorage.setItem('courses', JSON.stringify(courses));
-            //Show in table
-            tableCourses.addCourse(objCourse);
+            if (document.querySelector('#form').checkValidity()) {
+                //Create object
+                let objCourse = this._createObjCourse();
+                //Add and save in LocalStorange
+                courses.push(objCourse);
+                localStorage.setItem('courses', JSON.stringify(courses));
+                //Show in table
+                tableCourses.addCourse(objCourse);
+            }
+            else {
+                swal.fire({
+                    type: 'warning',
+                    title: 'Advertencia',
+                    text: 'Datos incompletos'
+                })
+            }
         });
     }
 
-    _createObjCourse(){
+    _createObjCourse() {
         //Format dates
         let stringStartDate = new Date(document.querySelector('#startDate').value);
         stringStartDate = (stringStartDate.getDate() + 1) + '/' + (stringStartDate.getMonth() + 1) + '/' + stringStartDate.getFullYear();
